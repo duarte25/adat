03fcad19ea9792b87da1996d4e7b7122933c5689
duarte25 <duarte.guga2025@gmail.com>
2024-07-17 15:48:14 -0400

    Incluindo req params em código e inlucindo novo padrão de cor em map

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
-"use client"
+"use client";
+
 import React, { useState } from "react";
 import { Chart } from "react-google-charts";
 import Filters from "../component/Filters";
 import Styles from "./page.module.css";
 import InputSelect from "../component/InputSelect";
-import { fetchApi } from "../utils/fetchApi.js";
+import { fetchApi } from "../utils/fetchApi";
 import { useQuery } from "react-query";
 
 // Mapeamento de siglas para nomes completos dos estados
@@ -40,28 +41,35 @@ const ufFullName = {
 
 export default function App() {
   const [metric, setMetric] = useState("Acidentes");
+  const [year, setYear] = useState("2022");
 
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["veiculoGetInformacoes"],
+    queryKey: ["veiculoGetInformacoes", year],
     queryFn: async () => {
-      const response = await fetchApi("/uf", "GET");
+      const response = await fetchApi(`/uf?dados=dados_uf_${year}`, "GET");
       return response;
     }
   });
 
-  const handleSelectChange = (event) => {
+  console.log(data)
+
+  const handleMetricChange = (event) => {
     setMetric(event.target.value);
+  };
+
+  const handleYearChange = (event) => {
+    setYear(event.target.value);
   };
 
   const getMetricData = (metric, data) => {
     const dataUf = [['State', metric]];
     if (data) {
       Object.keys(data).forEach(uf => {
-        const { count, total_death, total_involved } = data[uf];
+        const { total_accident, total_death, total_involved } = data[uf];
         const fullName = ufFullName[uf];
         if (fullName) {
           if (metric === "Acidentes") {
-            dataUf.push([fullName, count]);
+            dataUf.push([fullName, total_accident]);
           } else if (metric === "Óbitos") {
             dataUf.push([fullName, total_death]);
           } else if (metric === "Envolvidos") {
@@ -75,12 +83,16 @@ export default function App() {
 
   const options = {
     region: "BR",
+    displayMode: "regions",
     resolution: 'provinces',
-    colorAxis: { colors: ["#00853f", "black", "#e31b23"] },
-    backgroundColor: "#81d4fa",
-    datalessRegionColor: "#f8bbd0",
-    defaultColor: "#f5f5f5",
+    colorAxis: { colors: ["#70b0f5", "#083D77"] }, // Esquema de cores para destacar o Brasil
+    backgroundColor: "#F9F9F9", // Cor de fundo do mapa
+    datalessRegionColor: "#FFFFFF", // Cor para regiões sem dados (branco)
+    defaultColor: "#f5f5f5", // Cor padrão das regiões não destacadas
+    enableRegionInteractivity: true, // Ativar interatividade ao passar o mouse
+    magnifyingGlass: { enable: true, zoomFactor: 7.5 } // Lupa de zoom no Brasil
   };
+  
 
   const menuData = [
     { value: "Acidentes", label: "Acidentes" },
@@ -88,22 +100,44 @@ export default function App() {
     { value: "Envolvidos", label: "Envolvidos" },
   ];
 
+  const yearData = [
+    { value: "2018", label: "2018" },
+    { value: "2019", label: "2019" },
+    { value: "2020", label: "2020" },
+    { value: "2021", label: "2021" },
+    { value: "2022", label: "2022" },
+  ];
+
   return (
     <div className={Styles.container}>
       <Filters inputSelect={
-        <InputSelect
-          data={menuData}
-          selectLabel={"Metric"}
-          onChange={handleSelectChange}
-          value={metric}
-        />
+        <>
+          <InputSelect
+            data={menuData}
+            selectLabel={"Métrica"}
+            onChange={handleMetricChange}
+            value={metric}
+          />
+          <InputSelect
+            data={yearData}
+            selectLabel={"Ano"}
+            onChange={handleYearChange}
+            value={year}
+          />
+        </>
       } />
       <div className={Styles.map}>
-        <Chart
-          chartType="GeoChart"
-          data={getMetricData(metric, data)}
-          options={options}
-        />
+        {isLoading ? (
+          <p>Carregando...</p>
+        ) : isError ? (
+          <p>Erro: {error.message}</p>
+        ) : (
+          <Chart
+            chartType="GeoChart"
+            data={getMetricData(metric, data)}
+            options={options}
+          />
+        )}
       </div>
     </div>
   );
