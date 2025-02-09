d3aa9feaa01172896ca6e4f2c8c1afce7aeda93b
Gustavo Vinicius Duarte Da Silva <duarte.guga2025@gmail.com>
2025-02-09 14:23:07 -0400

    feat: Table feito

--- src/app/calendario/page.js
+++ src/app/calendario/page.js
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 
 import InputSelect from "../../component/InputSelect";
 import TablePhaseDay from "@/component/TablePhaseDay";
@@ -9,85 +9,102 @@ import React, { useState } from "react";
 import { useQuery } from "react-query";
 
 export default function Calendario() {
-  const [year, setYear] = useState("2022");
-  const [selectedYear, setSelectedYear] = useState("2022");
+    const [year, setYear] = useState("2022");
+    const [selectedYear, setSelectedYear] = useState("2022");
+    const [selectedSearch, setSelectedSearch] = useState("phase_day");
 
-  const { data, isLoading, isError, error, refetch } = useQuery({
-    queryKey: ["ufGetInformacoes", selectedYear],
-    queryFn: async () => {
-      const response = await fetchApi(`/phase_day?data=data_phase_day_${selectedYear}`, "GET");
-      return response;
-    },
-  });
+    const { data, isLoading, isError, error, refetch } = useQuery({
+        queryKey: ["ufGetInformacoes", selectedYear, selectedSearch],
+        queryFn: async () => {
+            const response = await fetchApi(`/${selectedSearch}?data=data_${selectedSearch}_${selectedYear}`, "GET");
+            return response;
+        },
+    });
 
-  const handleYearChange = (event) => {
-    setYear(event.target.value);
-  };
+    const handleYearChange = (event) => {
+        setYear(event.target.value);
+    };
 
-  const handleFetchData = () => {
-    setSelectedYear(year);
-    refetch();
-  };
+    const handleSearchChange = (event) => {
+        setSelectedSearch(event.target.value);
+    };
 
-  const getMetricData = (data) => {
-    const dataPhaseDay = [];
-    if (data) {
-      Object.keys(data).forEach(phaseDay => {
-        const { total_accident, total_death, total_involved, total_injured } = data[phaseDay];
-        dataPhaseDay.push({
-          phaseDay,
-          total_accident,
-          total_death,
-          total_involved,
-          total_injured
-        });
-      });
-    }
-    return dataPhaseDay;
-  };
+    const handleFetchData = () => {
+        setSelectedYear(year);
+        refetch();
+    };
 
-  const yearData = [
-    { value: "2018", label: "2018" },
-    { value: "2019", label: "2019" },
-    { value: "2020", label: "2020" },
-    { value: "2021", label: "2021" },
-    { value: "2022", label: "2022" },
-  ];
+    const getMetricData = (data) => {
+        const dataPhaseDay = [];
+        if (data) {
+            Object.keys(data).forEach(phaseDay => {
+                const { total_accident, total_death, total_involved, total_injured } = data[phaseDay];
+                dataPhaseDay.push({
+                    phaseDay,
+                    total_accident,
+                    total_death,
+                    total_involved,
+                    total_injured
+                });
+            });
+        }
+        return dataPhaseDay;
+    };
 
-  const tableData = getMetricData(data);
+    const yearData = [
+        { value: "2018", label: "2018" },
+        { value: "2019", label: "2019" },
+        { value: "2020", label: "2020" },
+        { value: "2021", label: "2021" },
+        { value: "2022", label: "2022" },
+    ];
 
-  return (
-    <div className="flex flex-col items-center pt-5 gap-5">
-      <div className="w-1/2">
-        <div className="flex flex-row mb-1">
-          <hr className="mr-1 bg-yale-blue h-5 w-1" />
-          <h2>ESTATÍSTICA DE ACIDENTES POR <strong>CLIMA E TEMPO</strong></h2>
+    const searchData = [
+        { value: "phase_day", label: "Fase do Dia" },
+        { value: "day_week", label: "Dia da Semana" },
+    ];
+
+    const tableData = getMetricData(data);
+
+    return (
+        <div className="flex flex-col items-center pt-5 gap-5">
+            <div className="w-1/2">
+                <div className="flex flex-row mb-1">
+                    <hr className="mr-1 bg-yale-blue h-5 w-1" />
+                    <h2>ESTATÍSTICA DE ACIDENTES POR <strong>CLIMA E TEMPO</strong></h2>
+                </div>
+                <Filters
+                    inputSelect={
+                        <>
+                            <InputSelect
+                                data={yearData}
+                                selectLabel={"Ano"}
+                                onChange={handleYearChange}
+                                value={year}
+                            />
+
+                            <InputSelect
+                                data={searchData}
+                                selectLabel={"Filtro"}
+                                onChange={handleSearchChange}
+                                value={selectedSearch}
+                            />
+                        </>
+                    }
+                    onButtonClick={handleFetchData}
+                />
+            </div>
+            <div className="h-4/5 w-4/5" >
+                {isLoading ? (
+                    <div className="flex justify-center items-center">
+                        <CircularProgress color="inherit" className="fixed z-[10] h-32 w-34" />
+                    </div>
+                ) : isError ? (
+                    <p>Erro: {error.message}</p>
+                ) : (
+                    <TablePhaseDay data={tableData} searchType={selectedSearch} />
+                )}
+            </div>
         </div>
-        <Filters
-          inputSelect={
-            <>
-              <InputSelect
-                data={yearData}
-                selectLabel={"Ano"}
-                onChange={handleYearChange}
-                value={year}
-              />
-            </>
-          }
-          onButtonClick={handleFetchData}
-        />
-      </div>
-      <div className="h-4/5 w-4/5" >
-        {isLoading ? (
-          <div className="flex justify-center items-center">
-            <CircularProgress color="inherit" className="fixed z-[10] h-32 w-34" />
-          </div>
-        ) : isError ? (
-          <p>Erro: {error.message}</p>
-        ) : (
-          <TablePhaseDay data={tableData} />
-        )}
-      </div>
-    </div>
-  );
+    );
 }
--- src/component/TablePhaseDay/index.jsx
+++ src/component/TablePhaseDay/index.jsx
@@ -1,32 +1,45 @@
+import { IoIosCloudyNight, IoMdAlert } from "react-icons/io";
 import TableContainer from '@mui/material/TableContainer';
+import { LuSearchX, LuSunrise } from "react-icons/lu";
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import { GiWindsock } from "react-icons/gi";
-import { IoMdAlert } from "react-icons/io";
-import { LuSearchX } from "react-icons/lu";
-import { SiDrizzle } from "react-icons/si";
-import { IoRainy } from "react-icons/io5";
+import { GiNightSky } from "react-icons/gi";
 import { MdSunny } from "react-icons/md";
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import * as React from 'react';
 
+// Mapeamento para phase_day
 const phaseDayMapping = {
+  "morning": { name: "Manhã", icon: <LuSunrise /> },
   "afternoon": { name: "Tarde", icon: <MdSunny /> },
-  "night": { name: "Noite", icon: <GiWindsock /> },
-  "dawn": { name: "Madrugada", icon: <SiDrizzle /> },
-  "morning": { name: "Manhã", icon: <IoRainy /> },
+  "night": { name: "Noite", icon: <IoIosCloudyNight /> },
+  "dawn": { name: "Madrugada", icon: <GiNightSky /> },
   "unknown": { name: "Desconhecido", icon: <LuSearchX /> },
   "not_informed": { name: "Não informado", icon: <IoMdAlert /> },
 };
 
-export default function TablePhaseDay({ data }) {
+// Mapeamento para day_week
+const dayWeekMapping = {
+  "sunday": { name: "Domingo" },
+  "monday": { name: "Segunda-feira" },
+  "tuesday": { name: "Terça-feira" },
+  "wednesday": { name: "Quarta-feira" },
+  "thursday": { name: "Quinta-feira" },
+  "friday": { name: "Sexta-feira" },
+  "saturday": { name: "Sábado" },
+};
+
+export default function TablePhaseDay({ data, searchType }) {
+
+  // Escolhe o mapeamento correto com base no tipo de busca
+  const mapping = searchType === "day_week" ? dayWeekMapping : phaseDayMapping;
 
   const mappedData = data.map(item => ({
     ...item,
-    phaseDay: phaseDayMapping[item.phaseDay] || { name: item.phaseDay, icon: null }
+    category: mapping[item.phaseDay] || mapping[item.dayWeek] || { name: item.phaseDay || item.dayWeek, icon: null }
   }));
 
   return (
@@ -34,23 +47,23 @@ export default function TablePhaseDay({ data }) {
       <Table aria-label="simple table">
         <TableHead>
           <TableRow className="bg-beige">
-            <TableCell className="text-xl">Clima</TableCell>
+            <TableCell className="text-xl">Categoria</TableCell>
             <TableCell className="text-xl" align="center">Acidentes</TableCell>
             <TableCell className="text-xl" align="center">Envolvidos</TableCell>
             <TableCell className="text-xl" align="center">Feridos</TableCell>
             <TableCell className="text-xl" align="center">Óbitos</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody className="bg-yale-blue" >
+        <TableBody className="bg-yale-blue">
           {mappedData.map((row) => (
-            <TableRow key={row.phaseDay.name}>
+            <TableRow key={row.category.name}>
               <TableCell className="text-snow-white text-xl" component="th" scope="row">
-                <div className="flex flex-row items-center content-center gap-1 text-xl" >
-                  {row.phaseDay.icon}
-                  <span>{row.phaseDay.name}</span>
+                <div className="flex flex-row items-center content-center gap-1 text-xl">
+                  {row.category.icon}
+                  <span>{row.category.name}</span>
                 </div>
               </TableCell>
-              <TableCell className="text-snow-white -yal text-xl" align="center">{row.total_accident}</TableCell>
+              <TableCell className="text-snow-white text-xl" align="center">{row.total_accident}</TableCell>
               <TableCell className="text-snow-white text-xl" align="center">{row.total_involved}</TableCell>
               <TableCell className="text-snow-white text-xl" align="center">{row.total_injured}</TableCell>
               <TableCell className="text-snow-white text-xl" align="center">{row.total_death}</TableCell>
